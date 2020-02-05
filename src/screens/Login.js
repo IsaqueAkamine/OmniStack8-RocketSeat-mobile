06bc01f2b7a303db8e147dcaf058831eac05d9d2
Isaque Akamine <isaqueakamine@gmail.com>
2020-02-05 14:56:33 -0300

    add API, handleLogin

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,13 +1,29 @@
-import React, { useState } from 'react';
-import { KeyboardAvoidingView, Image, View, Text, TextInput, TouchableOpacity, StyleSheet, Platform } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { KeyboardAvoidingView, Image, View, Text, TextInput, TouchableOpacity, StyleSheet, Platform, AsyncStorage } from 'react-native';
+
+import api from '../services/api';
 
 import logo from '../assets/logo.png';
 
 export default function Login({ navigation }) {
     const [user, setUser] = useState('');
 
-    function handleLogin() {
-        console.log(user);
+    useEffect(() => {
+        AsyncStorage.getItem('user').then(user => {
+            if (user) {
+                navigation.navigate('Main', { user })
+            }
+        })
+    }, [])
+
+    async function handleLogin() {
+        const response = await api.post('/devs', { username: user });
+
+        const { _id } = response.data;
+
+        await AsyncStorage.setItem('user', _id)
+        // console.log(_id);
+
         navigation.navigate('Main');
     }
 
