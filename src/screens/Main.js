d2f4375563e06132209d6c5ef8b3a03feb2c8262
Isaque Akamine <isaqueakamine@gmail.com>
2020-02-04 23:39:23 -0300

    add react navigation and create main screen

---
+++ src/screens/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+// import { Container } from './styles';
+
+export default function Main() {
+    return (
+        <View>
+            <Text>Pagina main</Text>
+        </View>
+    );
+}
